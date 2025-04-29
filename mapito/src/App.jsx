08524fd6920e02ec9a68a72b36597d8898baf13d
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-29 07:59:53 +0630

    LearnMore page components

--- mapito/src/App.jsx
+++ mapito/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from './components/Hero'
+import LearnMore from './components/LearnMore'
 
 const App = () => {
   return (
     <div>
+      <LearnMore />
       <Hero />
     </div>
   )
