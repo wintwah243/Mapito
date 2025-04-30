d651f3d9af201a9ebf8ca9d97a14833cc459babf
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-04-30 17:24:07 +0630

    Update SignUp.jsx

--- mapito/src/Auth/SignUp.jsx
+++ mapito/src/Auth/SignUp.jsx
@@ -98,7 +98,7 @@ const SignUp = () => {
   
 
     const loginWithGoogle = () => {
-      window.open("http://localhost:8000/api/auth/google/callback", "_self");
+      window.open("https://mapito.onrender.com/api/auth/google/callback", "_self");
     };
 
   return (
