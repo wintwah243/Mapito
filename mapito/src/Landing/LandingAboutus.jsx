900da1e805681bc56d90dfe42420d2e345571b45
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-05-01 13:55:19 +0630

    Update LandingAboutus.jsx

--- mapito/src/Landing/LandingAboutus.jsx
+++ mapito/src/Landing/LandingAboutus.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaCode, FaRobot, FaGraduationCap, FaLightbulb, FaChartLine } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const LandingAboutus = () => {
 
@@ -33,6 +34,7 @@ const LandingAboutus = () => {
 
   return (
     <>
+    <Navbar />
     <div className="mt-9 min-h-screen bg-gray-50">
       {/* Hero Section */}
       <section className="relative py-20 bg-white text-white">
