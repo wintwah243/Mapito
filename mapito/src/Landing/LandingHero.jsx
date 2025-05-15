4af14e217dc39213acfa35193380fdfdac7f8076
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-05-15 21:00:47 +0630

    modify UI

--- mapito/src/Landing/LandingHero.jsx
+++ mapito/src/Landing/LandingHero.jsx
@@ -30,7 +30,7 @@ const LandingHero = () => {
 
     return (
         <section id='hero' className="bg-white">
-            <div className="flex flex-col items-center justify-center min-h-screen px-4 py-8 sm:p-4">
+            <div className="flex flex-col items-center justify-center min-h-screen px-4 py-8 sm:p-4 mt-20">
                 {/* Header Section */}
                 <div className="w-full max-w-2xl text-center mb-8 px-2">
                     <h1 className="text-3xl sm:text-4xl font-bold mb-4">Plan your Roadmap with <span className='text-indigo-700'>AI</span></h1>
