9795e4c702b51978995bbc056bbd59b2a70e44d1
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-06-11 14:21:09 +0630

    ui update

--- mapito/src/components/Hero.jsx
+++ mapito/src/components/Hero.jsx
@@ -92,7 +92,7 @@ const Hero = () => {
     };
 
     return (
-        <section id='hero' className="bg-gradient-to-b from-gray-50 to-white">
+        <section id='hero' className="bg-white">
             <div className="flex flex-col items-center justify-center px-4 py-12 sm:p-6 lg:p-8">
                 {/* Header Section */}
                 <motion.div 
