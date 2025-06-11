01ba59434a709b056f2b968653cb2d42aa224cfa
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-06-11 14:26:26 +0630

    ui update

--- mapito/src/Auth/AuthLayout.jsx
+++ mapito/src/Auth/AuthLayout.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className="w-screen min-h-screen flex justify-center items-center bg-white p-4">
+    <div className="w-screen min-h-screen flex justify-center items-center bg-gray-100 p-4">
       <div className="w-full max-w-md bg-white p-8 md:p-12 rounded-2xl shadow-lg">
         <Link to="/login" className="flex items-center gap-3 mb-8 justify-center">
 {/*           <h2 className="text-4xl font-bold text-indigo-700">Mapito</h2>
