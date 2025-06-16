7f5467ed5a15d8e291341e21d5ceb2f06bdedbcb
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-06-16 16:43:44 +0630

    add backup file for bot's response

---
+++ mapito/src/utils/BackupBotInfo.js
@@ -0,0 +1,6 @@
+export const BackupBotInfo = `
+
+Mapito is an AI-powered learning and practicing platform built for aspiring and junior developers. It combines smart guidance with interactive tools to create a complete self-learning experience.
+
+At Mapito, we believe developers succeed faster when learning is structured, practical, and personalized — and we’re here to guide you all the way!
+`;
