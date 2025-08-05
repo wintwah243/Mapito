3fae65b1ae4686ba976d8d9afdff227482579720
Shi_Shi <142196738+wintwah243@users.noreply.github.com>
2025-08-05 16:57:30 +0630

    url change

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ passport.use(
   new OAuthStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback",
+    callbackURL: "https://mapito.onrender.com/api/auth/google/callback",
     scope: ["profile", "email"]
   },
     async (accessToken, refreshToken, profile, done) => {
