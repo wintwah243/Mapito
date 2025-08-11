37f8cb259f7a3fb3e46fd9cb682f41f6117fef30
ocean_shii <142196738+wintwah243@users.noreply.github.com>
2025-08-11 18:34:48 +0630

    Update main.jsx

--- mapito/src/main.jsx
+++ mapito/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AuthTokenHandler from '../src/Auth/AuthTokenHandler.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthTokenHandler>
     <App />
+    </AuthTokenHandler>
   </StrictMode>,
 )
